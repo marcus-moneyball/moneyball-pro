ce25c01aad8a51854b9be06c01850178908b8d31
Marcus-rodrigues <74578319+marcus-moneyball@users.noreply.github.com>
2026-09-07 19:41:24 -0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -30,33 +30,31 @@ try:
     from api.db import get_connection, fechar_conexao
     from api.projecao import obter_projecoes_partida
     from api.football_data_org import obter_forma_recente_estruturada
-    from api.odds_api_client import atualizar_cache_da_liga
-    from api.usuarios import checar_e_consumir_cota, sync_ghost_member, email_valido, LIMITE_CONSULTAS_FREE_DIARIO
-    from api.notificacoes_telegram import publicar_recomendacao_publica
-    from api.telegram_membros import (
-        gerar_token_vinculo, montar_link_vinculo, consumir_token_vinculo,
-        salvar_telegram_user_id, obter_plano_e_telegram,
-        enviar_convite_grupo_pro, remover_do_grupo_pro,
-    )
-    from api.ghost_admin import criar_membro_free_ghost
-except ImportError:
-    from catalogos import PERFIS_ANALISTA, CONFIG_MERCADO_PRINCIPAL
-    from calc import (
-        calcular_dossie, classificar_roteiro_jogo, calcular_matchup, calcular_convergencia,
-        calcular_aposta_combinada, ajustar_msc_por_convergencia, rotulo_confianca,
-    )
-    from mie1_gemini import get_gemini_client, extrair_mercados_estruturados, executar_mie1
-    from candidatos import (
-        montar_candidatos_over_under_calculados, montar_candidato_btts,
-        montar_candidato_moneyline, montar_candidatos_chance_dupla, montar_candidatos_handicap_asiatico,
-    )
-    from prompts_mie2 import montar_system_prompt_mie2
-    from validacao import validar_e_sanear_entrada
-    from utils import _parse_float_seguro
-    from db import get_connection, fechar_conexao
-    from projecao import obter_projecoes_partida
-    from football_data_org import obter_forma_recente_estruturada
-    from odds_api_client import atualizar_cache_da_liga
+    from .catalogos import PERFIS_ANALISTA, CONFIG_MERCADO_PRINCIPAL
+from .calc import (
+    calcular_dossie, classificar_roteiro_jogo, calcular_matchup, calcular_convergencia,
+    calcular_aposta_combinada, ajustar_msc_por_convergencia, rotulo_confianca,
+)
+from .mie1_gemini import get_gemini_client, extrair_mercados_estruturados, executar_mie1
+from .candidatos import (
+    montar_candidatos_over_under_calculados, montar_candidato_btts,
+    montar_candidato_moneyline, montar_candidatos_chance_dupla, montar_candidatos_handicap_asiatico,
+)
+from .prompts_mie2 import montar_system_prompt_mie2
+from .validacao import validar_e_sanear_entrada
+from .utils import _parse_float_seguro
+from .db import get_connection, fechar_conexao
+from .projecao import obter_projecoes_partida
+from .football_data_org import obter_forma_recente_estruturada
+from .odds_api_client import atualizar_cache_da_liga
+from .usuarios import checar_e_consumir_cota, sync_ghost_member, email_valido, LIMITE_CONSULTAS_FREE_DIARIO
+from .notificacoes_telegram import publicar_recomendacao_publica
+from .telegram_membros import (
+    gerar_token_vinculo, montar_link_vinculo, consumir_token_vinculo,
+    salvar_telegram_user_id, obter_plano_e_telegram,
+    enviar_convite_grupo_pro, remover_do_grupo_pro,
+)
+from .ghost_admin import criar_membro_free_ghost
     from usuarios import checar_e_consumir_cota, sync_ghost_member, email_valido, LIMITE_CONSULTAS_FREE_DIARIO
     from notificacoes_telegram import publicar_recomendacao_publica
     from telegram_membros import (
