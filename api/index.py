105322f718a9f2e97828fb437913e5917411d7e9
Marcus-rodrigues <74578319+marcus-moneyball@users.noreply.github.com>
2026-09-07 19:57:03 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,1 @@
-import sys
-import os
-
-# Força o diretório 'api' a ser a prioridade máxima de busca do Python
-dir_path = os.path.dirname(os.path.abspath(__file__))
-if dir_path not in sys.path:
-    sys.path.insert(0, dir_path)
-
-from main import app
+from api.main import app
